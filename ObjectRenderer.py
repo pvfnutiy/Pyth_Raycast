2c37b948015eb66a2a5b9e66006ebfd7d7c4c2a1
pafnutiy <105767751+pvfnutiy@users.noreply.github.com>
2023-05-25 11:24:26 +0700

    Initial commit

---
+++ ObjectRenderer.py
@@ -0,0 +1,37 @@
+import pygame as pg
+from settings import *
+
+class ObjectRenderer:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+        self.wall_textures = self.load_wall_texturess()
+        self.sky_image = self.get_texture('textures/sky.png', (WIDTH, HALF_HEIGHT))
+        self.sky_offset = 0
+
+    def draw(self):
+        self.draw_background()
+        self.render_game_object()
+
+    def draw_background(self):
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+
+        pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
+
+    def render_game_object(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
+    @staticmethod
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+        texture = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(texture, res)
+    
+    def load_wall_texturess(self):
+        return {
+            1: self.get_texture('textures/001.png'),
+            2: self.get_texture('textures/002.png'),
+        }
